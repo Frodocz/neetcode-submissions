cd36942d6cb4d5a2c9a6daf55e98c8961f6a3769
Zhang Chao <Frodocz@users.noreply.github.com>
2026-05-02 22:39:33 +0800

    Add: k-closest-points-to-origin - submission-2

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        auto cmp = [](vector<int>& p1, vector<int>& p2)
+        {
+            return p1[0] * p1[0] + p1[1] * p1[1] > p2[0] * p2[0] + p2[1] * p2[1];
+        };
+        priority_queue<vector<int>, vector<vector<int>>, decltype(cmp)> min_heap(cmp);
+        for (auto& p : points)
+        {
+            min_heap.push(p);
+        }
+        vector<vector<int>> ans;
+        ans.reserve(min_heap.size());
+        while (!min_heap.empty() && k--)
+        {
+            ans.push_back(min_heap.top());
+            min_heap.pop();
+        }
+        return ans;
+    }
+};
