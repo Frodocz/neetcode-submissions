b5a77c5c4ea550f309d1b2369536ea4a581bdad6
Zhang Chao <Frodocz@users.noreply.github.com>
2026-05-05 09:11:21 +0800

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    vector<vector<int>> memo;
+public:
+    int maxSubArray(vector<int>& nums) {
+        memo.assign(nums.size(), vector<int>(2, -1e7));
+        return dfs(nums, 0, false);
+    }
+
+    // flag = false => we have not started a subarray yet
+    // flag = true => we are currently building a subarray
+    int dfs(vector<int>& nums, int i, bool flag)
+    {
+        if (i == nums.size()) return flag ? 0 : -1e6;
+        if (memo[i][flag] != -1e7) return memo[i][flag];
+        if (flag)
+        {
+            memo[i][flag] = max(0, nums[i] + dfs(nums, i + 1, true));
+        }
+        else
+        {
+            memo[i][flag] = max(nums[i] + dfs(nums, i + 1, true),
+                   dfs(nums, i + 1, false));
+        }
+        return memo[i][flag];
+    }
+};
