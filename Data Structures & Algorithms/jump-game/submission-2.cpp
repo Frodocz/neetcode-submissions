2dbad7afb6704c3dc5d8a135d6dbd00fa1c87b46
Zhang Chao <Frodocz@users.noreply.github.com>
2026-05-05 12:43:29 +0800

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.cpp
@@ -0,0 +1,50 @@
+class Solution {
+    unordered_map<int, bool> memo;
+public:
+    bool canJump(vector<int>& nums) {
+        // memo[i] -> if we can reach end of nums at index i
+        // return dfs(nums, 0);
+
+        // dp[i] -> if at index i we can reach end of nums
+        int n = nums.size();
+        vector<bool> dp(n, false);
+        dp[n - 1] = true;
+        for (int i = n - 2; i >= 0; --i)
+        {
+            int end = min(n - 1, i + nums[i]);
+            for (int j = i + 1; j <= end; ++j)
+            {
+                if (dp[j])
+                {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[0];
+    }
+
+    bool dfs(vector<int>& nums, int i)
+    {
+        if (i == nums.size() - 1)
+        {
+            return true;
+        }
+
+        if (memo.count(i)) return memo[i];
+
+        if (nums[i] == 0) return false;
+
+        int end = min((int)nums.size() - 1, i + nums[i]);
+        for (int j = i + 1; j <= end; ++j)
+        {
+            if (dfs(nums, j))
+            {
+                memo[i] = true;
+                return true;
+            }
+        }
+        memo[i] = false;
+        return false;
+    }
+};
